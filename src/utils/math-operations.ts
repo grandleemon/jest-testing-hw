2a4b6d39946967d289d5851a4614f710ee27f227
Max <zeetreex@gmail.com>
2022-10-26 11:16:39 +0300

    subtract test succeed

--- src/utils/math-operations.ts
+++ src/utils/math-operations.ts
@@ -1,5 +1,5 @@
 const addition = (n1: string, n2: string) => String(+n1 + +n2)
-const subtract = (n1: string, n2: string) => ''
+const subtract = (n1: string, n2: string) => String(+n1 - +n2)
 
 export const math = (n1: string, operation: string, n2?: string) => {
     let result = '';
