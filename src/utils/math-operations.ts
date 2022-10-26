0cbbdbe77faa7bf3aa34135b00f61edd17e7e643
Max <zeetreex@gmail.com>
2022-10-26 11:23:49 +0300

    power test succeed

--- src/utils/math-operations.ts
+++ src/utils/math-operations.ts
@@ -1,7 +1,7 @@
 const addition = (n1: string, n2: string) => String(+n1 + +n2)
 const subtract = (n1: string, n2: string) => String(+n1 - +n2)
 const divide = (n1: string, n2: string) => String(+n1 / +n2)
-const power = (n1: string, n2: string) => ''
+const power = (n1: string, n2: string) => String(Math.pow(+n1, +n2))
 
 export const math = (n1: string, operation: string, n2?: string) => {
     let result = '';
