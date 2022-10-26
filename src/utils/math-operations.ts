8179d2798786af2802acaddbf9f17e479b934b91
Max <zeetreex@gmail.com>
2022-10-26 11:19:09 +0300

    divide test succeed

--- src/utils/math-operations.ts
+++ src/utils/math-operations.ts
@@ -1,6 +1,6 @@
 const addition = (n1: string, n2: string) => String(+n1 + +n2)
 const subtract = (n1: string, n2: string) => String(+n1 - +n2)
-const divide = (n1: string, n2: string) => ''
+const divide = (n1: string, n2: string) => String(+n1 / +n2)
 
 export const math = (n1: string, operation: string, n2?: string) => {
     let result = '';
