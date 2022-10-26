0bd9ec57f4a03f8144d99e0e66e8669357681671
Max <zeetreex@gmail.com>
2022-10-26 11:15:35 +0300

    subtract test failed

--- src/utils/math-operations.test.ts
+++ src/utils/math-operations.test.ts
@@ -4,3 +4,7 @@ import {expect} from "vitest";
 test('addition', () => {
     expect(math("1",'+', "2"), 'sum').toBe("3")
 })
+
+test('subtract', () => {
+    expect(math('3', '-', '1'), '').toBe("2")
+})
--- src/utils/math-operations.ts
+++ src/utils/math-operations.ts
@@ -1,9 +1,13 @@
 const addition = (n1: string, n2: string) => String(+n1 + +n2)
+const subtract = (n1: string, n2: string) => ''
 
 export const math = (n1: string, operation: string, n2?: string) => {
     let result = '';
 
+    console.log(operation)
+
     if(operation === '+' && n2) result = addition(n1, n2)
+    if(operation === '-' && n2) result = subtract(n1, n2)
 
     return result
 }
